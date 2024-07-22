0e445c38b7ff74c0c607d252f52ac314db38f4fd
EmilCh <emil@cheriches.com>
2024-07-22 23:38:50 +0200

    remove run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-sed -i -e "s#user: kemal#user: $DB_USER#" \
-       -e "s#password: kemal#password: $DB_PASSWORD#" \
-       -e "s#host: localhost#host: $DB_HOST#" \
-       -e "s#port: 5432#port: $DB_PORT#" \
-       -e "s#dbname: invidious#dbname: $DB_NAME#" \
-      /invidious/config/config.yml
-/invidious/invidious
